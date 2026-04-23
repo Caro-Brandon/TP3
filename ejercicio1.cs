2a4f31b3650c6d17972a9b82522f31d699117e8c
Caro-Brandon <1brandoncaro1@gmail.com>
2026-04-22 22:14:29 -0300

    shh

---
+++ ejercicio1.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class ejercicio 
+{
+     string nombre = "pablo";
+     int edad = 5;
+     double altura= 1.90;
+     string ciudad = "caba";
+
+    var nse1 = 5;
+     
+}
+
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        GetType(Nombre);
+        Console.ReadKey();
+    }
+}
