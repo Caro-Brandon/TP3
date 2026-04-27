1e66719b1534deb63873cbe41eeb6d4315c5f19b
Caro-Brandon <1brandoncaro1@gmail.com>
2026-04-26 21:36:48 -0300

    nse

--- ejercicio1.cs
+++ ejercicio1.cs
@@ -1,22 +1,47 @@
 using System;
 
-public class ejercicio 
+class Program
 {
-     string nombre = "pablo";
-     int edad = 5;
-     double altura= 1.90;
-     string ciudad = "caba";
-
-    var nse1 = 5;
-     
-}
-
-
-class Program 
-{
-    static void Main(string[] args) 
+    static void Main()
     {
-        GetType(Nombre);
+        string nombre = "Brandon";
+        int edad = 17;
+        double altura = 1.68;
+        string ciudad = "caba";
+
+        var numero = 10;
+        var texto = "Hola";
+        var decimalNum = 2.12;
+
+        Console.WriteLine(numero.GetType());
+        Console.WriteLine(texto.GetType());
+        Console.WriteLine(decimalNum.GetType());
+
+        bool esMayorDeEdad;
+        if(edad >= 18)
+        {
+            esMayorDeEdad = true;
+        }
+        else
+        {
+            esMayorDeEdad = false;
+        }
+
+        char inicial = 'B';
+
+        Console.WriteLine(nombre);
+        Console.WriteLine(edad);
+        Console.WriteLine(altura);
+        Console.WriteLine(ciudad);
+        Console.WriteLine(esMayorDeEdad);
+        Console.WriteLine(inicial);
+
+        Console.WriteLine($"Me llamo {nombre} y tengo {edad} años");
+
+        //int texto = "para ver un error xd ";
+        //string noPonerNda;
+        //Console.WriteLine(noPonerNda);
+        
         Console.ReadKey();
     }
 }
